58188cd4ee1cceb20892bb5bd678ebb045807dc8
Griffin Hammer <grifhammer@gmail.com>
2015-11-11 18:49:57 -0500

    Modularized code
    Modularized code in order to improve readability and to make it more
    reusable for the future.

--- scripts.js
+++ scripts.js
@@ -1,47 +1,52 @@
 $(document).ready(function(){
-	var steamApiBaseUrl = 'https://api.steampowered.com/'
-	var getTeamInfo = 'IDOTA2Match_570/GetTeamInfoByTeamID/v001/'
+	var steamApiBaseUrl = 'https://api.steampowered.com/';
+	var getTeamInfo = 'IDOTA2Match_570/GetTeamInfoByTeamID/v001/';
+	var getMatchesUrl = "IDOTA2Match_570/GetMatchHistory/v001/";
 	var apiKeyUrl = '?key=' + apiKey;
-	var languageUrl = ''
-	var format = '&format=json'
-	var startAtThisTeamId = "2586976"
-	var startingTeamUrl = "&start_at_team_id=" + startAtThisTeamId;
-	var numRequestedTeams = 1;
-	var teamsRequestedUrl = '&teams_requested=' + numRequestedTeams;
-	var getMatchesUrl = "IDOTA2Match_570/GetMatchHistory/v001/"
-	var teamOGId = startAtThisTeamId;
-	var steamApiCall = steamApiBaseUrl + getTeamInfo + apiKeyUrl + startingTeamUrl + teamsRequestedUrl;
+	var format = '&format=json';
 
-	var results = getFakeJson(steamApiCall);
+	var teamOGId = "2586976";
+
+	var startAtThisTeamId = teamOGId;
+	var startingTeamUrl = "&start_at_team_id=" + startAtThisTeamId;
+
+	var numRequestedTeams = 1;
+	var numTeamsRequestedUrl = '&teams_requested=' + numRequestedTeams;
+
+	var steamApiCall = steamApiBaseUrl + getTeamInfo + apiKeyUrl + startingTeamUrl + numTeamsRequestedUrl;
+
+	var teamJsonResults = getFakeJson(steamApiCall);
+
 	var teamOGIds = []
-	var teamOGMatchIds =[];
-	teamOGIds.push(results.result.teams[0].team_id);
-	teamOGIds.push('2519319');
+	var teamOGMatchIds = [];
+
+	teamOGIds = getTeamIds(teamJsonResults);
+
+
 
 	var teamOGMatches = []
-	for(var teamIdIndex = 0; teamIdIndex < teamOGIds.length; teamIdIndex++){
-		teamOGId = teamOGIds[teamIdIndex];
-		//get the matches from leagues they play in
-		for(var leagueIndex = 0; leagueIndex < teamOGLeagues.length; leagueIndex++){
-			var leagueId = teamOGLeagues[leagueIndex].leagueid;
-			var leagueIdUrl = '&league_id=' + leagueId;
-			steamApiCall = steamApiBaseUrl + getMatchesUrl + apiKeyUrl + leagueIdUrl;
 
 
-			leagueMatchesJSON = getFakeJson(steamApiCall);
+	
+	//get the matches from leagues they play in
+	for(var leagueIndex = 0; leagueIndex < teamOGLeagues.length; leagueIndex++){
+		var leagueId = teamOGLeagues[leagueIndex].leagueid;
+		var leagueIdUrl = '&league_id=' + leagueId;
+		steamApiCall = steamApiBaseUrl + getMatchesUrl + apiKeyUrl + leagueIdUrl;
+
+
+		leagueMatchesJSON = getFakeJson(steamApiCall);
+		for(var teamIdIndex = 0; teamIdIndex < teamOGIds.length; teamIdIndex++){
+			teamOGId = teamOGIds[teamIdIndex];
 			var leagueMatches = leagueMatchesJSON.result.matches;
-
+			var teamOGMatchObj = findGamesPlayedByTeamID(teamOGIds[teamIdIndex], leagueMatches);
+			teamOGIds = teamOGMatchObj.matchIds;
+			teamOGMatches = teamOGMatchObj.matches;
 			//get match IDs for matches that they actually played in
-			for(var matchesIndex = 0; matchesIndex < leagueMatches.length; matchesIndex++){
-				if(teamOGId == leagueMatches[matchesIndex].dire_team_id ||
-					teamOGId == leagueMatches[matchesIndex].radiant_team_id){
-					teamOGMatches.push(leagueMatches[matchesIndex]);
-					teamOGMatchIds.push(leagueMatches[matchesIndex].match_id)
-				}
-			}
+			
 		}
 	}
-	
+
 
 
 	for(var matchIdIndex = 0; matchIdIndex < teamOGMatchIds.length; matchIdIndex++){
@@ -54,7 +59,28 @@ $(document).ready(function(){
 	}
 	
 
+	function getTeamIds(teamJson){
+		var idArray;
+		idArray.push(teamJson.result.teams[0].team_id);
+		idArray.push("2519319");
+		return idArray;
+	}
 
+	function findGamesPlayedByTeamID(teamId, matchArray){
+		var teamMatches = [];
+		var teamMatchIds = [];
+		for(var matchesIndex = 0; matchesIndex < matchArray.length; matchesIndex++){
+			if(TeamId == matchArray[matchesIndex].dire_team_id ||
+				TeamId == matchArray[matchesIndex].radiant_team_id){
+				teamMatches.push(matchArray[matchesIndex]);
+				teamMatchIds.push(matchArray[matchesIndex].match_id)
+			}
+		}
+		return {
+			matches: teamMatches,
+			matchIds: teamMatchIds
+		}
+	}
 });
 
 function getFakeJson(JSON){
