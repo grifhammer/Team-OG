4ea99784c6412190fd194c34622c47bfd27f9963
Griffin Hammer <grifhammer@gmail.com>
2015-12-02 09:45:35 -0500

    Added player images.

--- team-controller.js
+++ team-controller.js
@@ -9,9 +9,9 @@ teamApp.controller('teamController', function($scope){
 		this.role = role;
 	}
 
-	$scope.playerList.push(new Player("Johan Sundstein", "n0tail", "", "Carry", false));
+	$scope.playerList.push(new Player("Johan Sundstein", "n0tail", "n0tail.jpeg", "Carry", false));
 	$scope.playerList.push(new Player("Amer Barqawi", "Miracle-", "", "Mid", false));
-	$scope.playerList.push(new Player("David Tan", "Moon", "", "Offlane", false));
-	$scope.playerList.push(new Player("Andreas Nielsen", "Cr1t-", "", "Support(4)", false));
-	$scope.playerList.push(new Player("Tal Aizik", "Fly", "", "Support(5)", true));
+	$scope.playerList.push(new Player("David Tan", "Moon", "moon.jpeg", "Offlane", false));
+	$scope.playerList.push(new Player("Andreas Nielsen", "Cr1t-", "crit-.jpeg", "Support(4)", false));
+	$scope.playerList.push(new Player("Tal Aizik", "Fly", "fly.jpeg", "Support(5)", true));
 });
